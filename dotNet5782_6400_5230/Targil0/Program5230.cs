d6db4d49db0738874035b27a339b6b60bc30a1c2
מרים חנה בלה <mbella@jct.ac.il>
2021-10-07 23:00:24 +0300

    i did the part.

---
+++ dotNet5782_6400_5230/Targil0/Program5230.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static partial void Welcome5230()
+        {
+            Console.WriteLine("I am here!");
+        }
+    }
+}
