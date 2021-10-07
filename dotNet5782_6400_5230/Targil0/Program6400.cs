d3d902b5f000afba048ee993295816402f7d904c
rjkcrkhi <rberlin@g.jct.ac.il>
2021-10-07 22:34:31 +0300

    Initial code + Welcome function

---
+++ dotNet5782_6400_5230/Targil0/Program6400.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6400();
+            Welcome5230();
+            Console.ReadKey();
+        }
+        static partial void Welcome5230();
+        private static void Welcome6400()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
