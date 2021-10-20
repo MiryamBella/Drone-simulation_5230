e888daf491b5642ba65941be5fe5443e5efb13c2
מרים חנה בלה <mbella@jct.ac.il>
2021-10-20 17:52:28 +0300

    i didnt finish

--- dotNet5782_6400_5230/DAL/DataSource.cs
+++ dotNet5782_6400_5230/DAL/DataSource.cs
@@ -7,23 +7,30 @@ namespace DalObject
 {
     internal class DataSource
     {
-        List<Quadocopter> l;
-        
+        // i warite arrys and not lists because i think it will be more easy for us to get the data.
+        internal static Quadocopter[] qpter = new Quadocopter[10];
+        internal static BaseStation[] bstion = new BaseStation[5];
+        internal static Client[] cli = new Client[100];
+        static Packagh[] packagh = new Packagh[1000];
 
-
-        internal class Config //new evrething in this class.
+        internal static class Config //new evrething in this class.
         {
-            //i write all the indexs if we will changh our maind and change the lists to arry.
-            int index_Quadocopter = 0;
-            int index_baseStation = 0;
-            int index_client = 0;
-            int index_packagh = 0;
+            //i write all the indexs if we will.
+            static int index_quadocopter = 0;
+            static int index_baseStation = 0;
+            static int index_client = 0;
+            static int index_packagh = 0;
 
-            int runNum = 0;//for the id packagh.
-
-            static void Initialize() { 
+            static int runNum = 0;//for the id packagh.
+                                  //i did the integger as static becuse i think it need to be only one like that because any id need to be difrent.
+                           
             
-            }
+        }
+        static void Initialize()
+        {
+            packagh[0] = new Packagh();
+            index_packagh++;
+
         }
 
     }
