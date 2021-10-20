6d4bd01514cd279627d0e15e1bbbc99f0b6d36da
מרים חנה בלה <mbella@jct.ac.il>
2021-10-20 15:43:16 +0300

    i put conection to the  namspace do

--- dotNet5782_6400_5230/DAL/DataSource.cs
+++ dotNet5782_6400_5230/DAL/DataSource.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using IDAL.DO;
 
 namespace DalObject
 {
     internal class DataSource
     {
-        Quadocopter quadocopter;
-        //List<Quadocopter> l =new List<Quadocopter>;
+        List<Quadocopter> l;
+        
 
 
-        internal class Config
+        internal class Config //new evrething in this class.
         {
+            //i write all the indexs if we will changh our maind and change the lists to arry.
+            int index_Quadocopter = 0;
+            int index_baseStation = 0;
+            int index_client = 0;
+            int index_packagh = 0;
 
+            int runNum = 0;//for the id packagh.
+
+            static void Initialize() { 
+            
+            }
         }
 
     }
