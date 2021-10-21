962bbe775d2bfb1931a7aed2d4319862da597706
מרים חנה בלה <mbella@jct.ac.il>
2021-10-21 13:39:34 +0300

    i think i finish the class. if you think i need to change thinks or to write more things, tell me.

--- dotNet5782_6400_5230/DAL/DataSource.cs
+++ dotNet5782_6400_5230/DAL/DataSource.cs
@@ -5,15 +5,17 @@ using IDAL.DO;
 
 namespace DalObject
 {
-    internal class DataSource
+    internal class DataSource // evrething is new in this class.
     {
-        // i warite arrys and not lists because i think it will be more easy for us to get the data.
+ 
+        /* i warite arrys and not lists because i think it will be more easy for us to get the data.
+         */
         internal static Quadocopter[] qpter = new Quadocopter[10];
         internal static BaseStation[] bstion = new BaseStation[5];
         internal static Client[] cli = new Client[100];
         static Packagh[] packagh = new Packagh[1000];
 
-        internal static class Config //new evrething in this class.
+        internal class Config
         {
             //i write all the indexs if we will.
             internal static int index_quadocopter = 0;
@@ -28,9 +30,37 @@ namespace DalObject
         }
         static void Initialize()
         {
-            packagh[0] = new Packagh();
-            index_packagh++;
+            //i reset the arrys.
+            bstion[Config.index_baseStation++] = new BaseStation();//1
+            bstion[Config.index_baseStation++] = new BaseStation();//2
 
+            qpter[Config.index_quadocopter++] = new Quadocopter();//1
+            qpter[Config.index_quadocopter++] = new Quadocopter();//2
+            qpter[Config.index_quadocopter++] = new Quadocopter();//3
+            qpter[Config.index_quadocopter++] = new Quadocopter();//4
+            qpter[Config.index_quadocopter++] = new Quadocopter();//5
+
+            cli[Config.index_client++] = new Client();//1
+            cli[Config.index_client++] = new Client();//2
+            cli[Config.index_client++] = new Client();//3
+            cli[Config.index_client++] = new Client();//4
+            cli[Config.index_client++] = new Client();//5
+            cli[Config.index_client++] = new Client();//6
+            cli[Config.index_client++] = new Client();//7
+            cli[Config.index_client++] = new Client();//8
+            cli[Config.index_client++] = new Client();//9
+            cli[Config.index_client++] = new Client();//10
+
+            packagh[Config.index_packagh++] = new Packagh();//1
+            packagh[Config.index_packagh++] = new Packagh();//2
+            packagh[Config.index_packagh++] = new Packagh();//3
+            packagh[Config.index_packagh++] = new Packagh();//4
+            packagh[Config.index_packagh++] = new Packagh();//5
+            packagh[Config.index_packagh++] = new Packagh();//6
+            packagh[Config.index_packagh++] = new Packagh();//7
+            packagh[Config.index_packagh++] = new Packagh();//8
+            packagh[Config.index_packagh++] = new Packagh();//9
+            packagh[Config.index_packagh++] = new Packagh();//10
         }
 
     }
