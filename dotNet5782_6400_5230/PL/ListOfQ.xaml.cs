38737e2ee7a733c6f49b48c94c913c32e30b3330
מרים חנה בלה <mbella@jct.ac.il>
2021-12-08 01:07:54 +0200

    i only did the isul stafh

--- dotNet5782_6400_5230/PL/ListOfQ.xaml.cs
+++ dotNet5782_6400_5230/PL/ListOfQ.xaml.cs
@@ -21,16 +21,44 @@ namespace PL
     public partial class ListOfQ : Window
     {
         IBL.IBL bL;
+        int numOf_q;
+        private List<myData> myCollection = new List<myData>();
         public ListOfQ(IBL.IBL ibl)
         {
             InitializeComponent();
-            bL = new BL();
-            bL = ibl;
+            //bL = new BL();
+            //bL = ibl;
+            numOf_q = 0;
+            DataContext = myCollection;
+            myCollection.Add(new myData { id = 1 });
+            for (int i = 0; i < 10; ++i)
+            {
+                ListBoxItem newItem = new ListBoxItem();
+                newItem.Content = "Quadocopter " + (i + 1).ToString();
+                q_list.Items.Add(newItem);
+
+            }
+
         }
 
         private void addNew_Q(object sender, RoutedEventArgs e)
         {
-            new Quadocopter(bL).Show();
+            //new Quadocopter(bL).ShowDialog();
+            //ListBoxItem newItem = new ListBoxItem();
+            //newItem.Content = "Quadocopter " + numOf_q.ToString()+':';
+            //q_list.Items.Add(newItem);
+            //q_list
+            //data
         }
+
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            //this.Close();
+        }
+    }
+
+    public class myData
+    {
+        public int id;
     }
 }
