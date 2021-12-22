c21a9bb8246edeb3e80d998f00cc3604fc7b5886
rjkcrkhi <rberlin@g.jct.ac.il>
2021-12-22 11:51:54 +0200

    clients

---
+++ dotNet5782_6400_5230/PL/Client/Client.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Client
+{
+    /// <summary>
+    /// Interaction logic for Client.xaml
+    /// </summary>
+    public partial class Client : Window
+    {
+        public Client(BlApi.IBL bl)
+        {
+            InitializeComponent();
+        }
+        public Client(BlApi.IBL bl,BO.ClientToList c)
+        {
+            InitializeComponent();
+        }
+    }
+}
